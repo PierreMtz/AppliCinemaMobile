80c4c302f347268fa14e9472fae31aef731de63e
PierreM <PierreM@ASTIDOM.COM>
2021-05-14 16:57:01 +0200

    commit 2

--- myReactNativeProject/App.js
+++ myReactNativeProject/App.js
@@ -1,48 +1,62 @@
-import * as React from "react";
+// App.js
 
-import { Card } from "react-native-elements";
-import { View, Image,Text,ImageBackground } from "react-native";
-import { render } from "react-dom";
+import React from 'react'
+import Search from './Components/Search'
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      films: []
-    }
-  }
-
-  componentDidMount(){
-    console.log('test1');
-    fetch('https://192.168.1.17:5001/films', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    })
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      this.setState({
-        films: data
-      }, console.log('test'))
-    })
-    .catch(err => {
-      console.log(err)
-    });
-  }
-
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
-      <ImageBackground source={'https://img.imageboss.me/cdn/https://www.sleeplessmedia.com/wp-content/themes/sleepless/includes/img/homeLumeStars.jpg'} style={{width: '100%', height: '100%'}}>
-      <Text>Inside</Text>
-     </ImageBackground>
-      
-    );
+      <Search/>
+    )
   }
 }
 
-export default App;
+// import * as React from "react";
+
+// import { Card } from "react-native-elements";
+// import { View, Image,Text,ImageBackground } from "react-native";
+// import { render } from "react-dom";
+
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+    
+//     this.state = {
+//       films: [], qte: 1 
+//     }
+//     this.render()
+//   }
+
+//   componentDidMount(){
+//     console.log('test1');
+//     fetch('https://192.168.1.17:5001/films', {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//       }
+//     })
+//     .then(response => {
+//       return response.json()
+//     })
+//     .then(data => {
+//       this.setState({
+//         films: data
+//       }, console.log('test'))
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     });
+//   }
+
+//   render(){
+//     return (
+//     //   <ImageBackground source={'https://img.imageboss.me/cdn/https://www.sleeplessmedia.com/wp-content/themes/sleepless/includes/img/homeLumeStars.jpg'} style={{width: '100%', height: '100%'}}>
+//     //   <Text>Inside</Text>
+//       //  </ImageBackground>
+//       <Text>Hello !</Text>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ myReactNativeProject/Helpers/filmsData.js
@@ -0,0 +1,33 @@
+// Helpers/filmsData.js
+
+export default data = [
+    {
+       id:181808,
+       vote_average:7.2,
+       title:"Joker",
+       poster_path:"https://m.media-amazon.com/images/I/61BGvE8DumL._AC_SY606_.jpg",
+       original_title:"Joker",
+       overview:"Dans les années 1980, à Gotham City, Arthur Fleck, un comédien de stand-up raté est agressé alors qu'il ère dans les rues de la ville déguisé en clown. Méprisé de tous et bafoué, il bascule peu à peu dans la folie pour devenir le Joker, un dangereux tueur psychotique.",
+       release_date:"02/10/2019"
+    },
+    {
+       id:181809,
+       vote_average:8.1,
+       title:"Avatar",
+       poster_path:"https://img.auctiva.com/imgdata/1/9/8/3/0/8/8/webimg/890241618_o.jpg",
+       original_title:"Avatar",
+       overview:"Malgré sa paralysie, Jake Sully, un ancien marine immobilisé dans un fauteuil roulant, est resté un combattant au plus profond de son être. Il est recruté pour se rendre à des années-lumière de la Terre, sur Pandora, où de puissants groupes industriels exploitent un minerai rarissime destiné à résoudre la crise énergétique sur Terre.",
+       release_date:"16/12/2009"
+    },
+    {
+       id:181809,
+       vote_average:'??',
+       title:"Fast And Furious",
+       poster_path:"https://fr.web.img3.acsta.net/pictures/20/01/31/09/46/1825827.jpg",
+       original_title:"Fast And Furious",
+       overview:"Dominic Toretto et son équipe unissent leurs forces pour combattre l'assassin le plus habile et le conducteur le plus performant qu'ils aient jamais rencontré : son frère abandonné.",
+       release_date:"XX/06/2021"
+    }
+ ]
+ 
+ 
